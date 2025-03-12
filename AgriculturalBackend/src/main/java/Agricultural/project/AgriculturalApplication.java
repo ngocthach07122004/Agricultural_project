e3fa7a38d96071ddc1dacea8d2225969cfbde91a
ngoc thacch <huynhngocthach2004@gmail.com>
2025-03-12 17:24:32 +0700

    intitial agricultural web

---
+++ AgriculturalBackend/src/main/java/Agricultural/project/AgriculturalApplication.java
@@ -0,0 +1,13 @@
+package Agricultural.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriculturalApplication.class, args);
+	}
+
+}
