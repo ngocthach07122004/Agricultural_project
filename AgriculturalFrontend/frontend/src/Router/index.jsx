3615e44053112590e8680aab0b557dfb87e48b5f
ngoc thacch <huynhngocthach2004@gmail.com>
2025-03-27 04:20:15 +0700

    fix conflic in file Router

--- AgriculturalFrontend/frontend/src/Router/index.jsx
+++ AgriculturalFrontend/frontend/src/Router/index.jsx
@@ -5,30 +5,18 @@ import Signin from "../pages/Signin";
 import Signup from "../pages/Signup";
 import About from "../pages/About";
 import Profile from "../pages/UserProfile";
-<<<<<<< HEAD
-import Menu from "../pages/Menu";
-=======
+// <<<<<<< HEAD
+// import Menu from "../pages/Menu";
+// =======
 import Dashboard from "../pages/Dashboard";
 import Settings from "../pages/Settings";
 import Schedule from "../pages/Schedule"; // Import trang Schedule
 
->>>>>>> 82708c8 (Cập nhật các trang mới như Dashboard, Lên lịch và Cài đặt. Đồng thời xóa một số thứ khác...)
 const router = createBrowserRouter([
   {
     path: "/",
     element: <DefaultLayout />,
     children: [
-<<<<<<< HEAD
-      {
-        index: true,
-        element: <Home />,
-      },
-      { path: "signin/", element: <Signin /> },
-      { path: "signup/", element: <Signup /> },
-      { path: "about/", element: <About /> },
-      { path: "profile/", element: <Profile /> },
-      { path: "menu/", element: <Menu /> },
-=======
       { index: true, element: <Home /> },
       { path: "signin", element: <Signin /> },
       { path: "signup", element: <Signup /> },
@@ -37,7 +25,6 @@ const router = createBrowserRouter([
       { path: "dashboard", element: <Dashboard /> },
       { path: "settings", element: <Settings /> },
       { path: "schedule", element: <Schedule /> }, // Thêm route cho Schedule
->>>>>>> 82708c8 (Cập nhật các trang mới như Dashboard, Lên lịch và Cài đặt. Đồng thời xóa một số thứ khác...)
     ],
   },
 ]);
