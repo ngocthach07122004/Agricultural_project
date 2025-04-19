a3deffa04b570d1303e7bd707fdfbd75e6c2891e
ngoc thacch <huynhngocthach2004@gmail.com>
2025-04-19 11:10:12 +0700

    connect module AI/Fruit-Quality AI/MangoLeaf to frontend

--- AgriculturalFrontend/frontend/src/pages/FruitQuality/index.jsx
+++ AgriculturalFrontend/frontend/src/pages/FruitQuality/index.jsx
@@ -17,10 +17,11 @@ import {
   FaCheckCircle,
   FaExclamationCircle,
 } from "react-icons/fa";
+import { useState } from "react";
 
 const cx = classNames.bind(styles);
 
-const response = {
+const responseFakeData = {
   fruit: {
     prediction: "Apple",
     confidence: 99.97,
@@ -50,70 +51,148 @@ const transformData = (probabilities) =>
   }));
 
 function FruitQuality() {
+  const [imageSrc, setImageSrc] = useState(null);
+  const [response, setResponse] = useState(responseFakeData);
   const fruitData = transformData(response.fruit.probabilities);
   const qualityData = transformData(response.quality.probabilities);
+  const [filePicture, setFilePicture] = useState(null);
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFilePicture(file);
+      const previewUrl = URL.createObjectURL(file);
+      setImageSrc(previewUrl);
+    }
+  };
+  const handleOnClickUploadFile = async () => {
+    if (!filePicture) {
+      alert("Vui lòng chọn một file ảnh.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", filePicture);
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/predict/", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+        },
+        body: formData,
+      });
+
+      const data = await response.json();
+      setResponse(data);
+      console.log("abc");
+    } catch (error) {
+      console.error("Lỗi khi gửi ảnh:", error);
+      alert("Gửi ảnh thất bại!");
+    }
+    // finally {
+    //   setLoading(false);
+    // }
+  };
   return (
-    <div className={cx("wrapper")}>
-      <div className={cx("section")}>
-        <div className={cx("title")}>
-          <FaAppleAlt className={cx("iconHeader")} />
-          Dự đoán loại trái cây
-        </div>
-        <div className={cx("description")}>
-          <FaCheckCircle color="#2ecc71" /> Kết quả:{" "}
-          <strong>{response.fruit.prediction}</strong> (
-          <FaPercent /> {response.fruit.confidence.toFixed(2)}%)
-        </div>
-        <div className={cx("chart")}>
-          <ResponsiveContainer width="90%" height={250}>
-            <BarChart data={fruitData}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis domain={[0, 100]} />
-              <Tooltip />
-              <Bar dataKey="value" fill="#82ca9d" radius={[10, 10, 0, 0]} />
-            </BarChart>
-          </ResponsiveContainer>
+    <div>
+      <div className={cx("container_uploadFile")}>
+        <div className={cx("wrapper_uploadFile")}>
+          <h3 className={cx("wrapper_uploadFile_title")}>
+            {" "}
+            Upload ảnh để tiến hành dự đoán
+          </h3>
+          <div className={cx("wrapper_uploadFile_forInput")}>
+            <input
+              className={cx("wrapper_uploadFile_input")}
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+            />
+          </div>
+          <div className={cx("wrapper_uploadFile_forButton")}>
+            <button
+              className={cx("wrapper_uploadFile_button")}
+              onClick={handleOnClickUploadFile}
+            >
+              {" "}
+              Phân tích{" "}
+            </button>
+          </div>
+          <div className={cx("wrapper_imageUpload_parent")}>
+            {imageSrc && (
+              <img
+                className={cx("wrapper_imageUpload")}
+                src={imageSrc}
+                alt="preview"
+              />
+            )}
+          </div>
         </div>
       </div>
+      <div className={cx("wrapper")}>
+        <div className={cx("section")}>
+          <div className={cx("title")}>
+            <FaAppleAlt className={cx("iconHeader")} />
+            Dự đoán loại trái cây
+          </div>
+          <div className={cx("description")}>
+            <FaCheckCircle color="#2ecc71" /> Kết quả:{" "}
+            <strong>{response.fruit.prediction}</strong> (
+            <FaPercent /> {response.fruit.confidence.toFixed(2)}%)
+          </div>
+          <div className={cx("chart")}>
+            <ResponsiveContainer width="90%" height={250}>
+              <BarChart data={fruitData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis domain={[0, 100]} />
+                <Tooltip />
+                <Bar dataKey="value" fill="#82ca9d" radius={[10, 10, 0, 0]} />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
 
-      <div className={cx("section")}>
-        <div className={cx("title")}>
-          {response.quality.prediction === "Good" ? (
-            <FaThumbsUp className={cx("iconHeader")} />
-          ) : (
-            <FaThumbsDown className={cx("iconHeader")} />
-          )}
-          Dự đoán chất lượng
-        </div>
-        <div className={cx("description")}>
-          {response.quality.prediction === "Good" ? (
-            <FaCheckCircle color="#2ecc71" />
-          ) : (
-            <FaExclamationCircle color="#e74c3c" />
-          )}{" "}
-          Kết qủa :{" "}
-          <strong
-            style={{
-              color:
-                response.quality.prediction === "Good" ? "#27ae60" : "#e74c3c",
-            }}
-          >
-            {response.quality.prediction}
-          </strong>{" "}
-          (<FaPercent /> {response.quality.confidence.toFixed(2)}%)
-        </div>
-        <div className={cx("chart")}>
-          <ResponsiveContainer width="90%" height={250}>
-            <BarChart data={qualityData}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis domain={[0, 100]} />
-              <Tooltip />
-              <Bar dataKey="value" fill="#f66f91" radius={[10, 10, 0, 0]} />
-            </BarChart>
-          </ResponsiveContainer>
+        <div className={cx("section")}>
+          <div className={cx("title")}>
+            {response.quality.prediction === "Good" ? (
+              <FaThumbsUp className={cx("iconHeader")} />
+            ) : (
+              <FaThumbsDown className={cx("iconHeader")} />
+            )}
+            Dự đoán chất lượng
+          </div>
+          <div className={cx("description")}>
+            {response.quality.prediction === "Good" ? (
+              <FaCheckCircle color="#2ecc71" />
+            ) : (
+              <FaExclamationCircle color="#e74c3c" />
+            )}{" "}
+            Kết qủa :{" "}
+            <strong
+              style={{
+                color:
+                  response.quality.prediction === "Good"
+                    ? "#27ae60"
+                    : "#e74c3c",
+              }}
+            >
+              {response.quality.prediction}
+            </strong>{" "}
+            (<FaPercent /> {response.quality.confidence.toFixed(2)}%)
+          </div>
+          <div className={cx("chart")}>
+            <ResponsiveContainer width="90%" height={250}>
+              <BarChart data={qualityData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis domain={[0, 100]} />
+                <Tooltip />
+                <Bar dataKey="value" fill="#f66f91" radius={[10, 10, 0, 0]} />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
         </div>
       </div>
     </div>
--- AgriculturalFrontend/frontend/src/pages/LeafMango/index.jsx
+++ AgriculturalFrontend/frontend/src/pages/LeafMango/index.jsx
@@ -10,10 +10,11 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import { FaLeaf, FaMicroscope } from "react-icons/fa";
+import { useState } from "react";
 
 const cx = classNames.bind(styles);
 
-const response = {
+const responseFakeData = {
   prediction: "Powdery Mildew",
   confidence: 99.49,
   probabilities: {
@@ -35,29 +36,104 @@ const transformData = (probabilities) =>
   }));
 
 function LeafMango() {
+  const [imageSrc, setImageSrc] = useState(null);
+  const [response, setResponse] = useState(responseFakeData);
   const chartData = transformData(response.probabilities);
+  const [filePicture, setFilePicture] = useState(null);
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFilePicture(file);
+      const previewUrl = URL.createObjectURL(file);
+      setImageSrc(previewUrl);
+    }
+  };
+  const handleOnClickUploadFile = async () => {
+    if (!filePicture) {
+      alert("Vui lòng chọn một file ảnh.");
+      return;
+    }
 
+    const formData = new FormData();
+    formData.append("file", filePicture);
+
+    try {
+      const response = await fetch("http://127.0.0.1:8001/predict/", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+        },
+        body: formData,
+      });
+
+      const data = await response.json();
+      setResponse(data);
+      console.log("abc");
+    } catch (error) {
+      console.error("Lỗi khi gửi ảnh:", error);
+      alert("Gửi ảnh thất bại!");
+    }
+    // finally {
+    //   setLoading(false);
+    // }
+  };
   return (
-    <div className={cx("wrapper")}>
-      <div className={cx("title")}>
-        <FaLeaf className={cx("icon")} />
-        Phát hiện bệnh lá xoài
+    <div>
+      <div className={cx("container_uploadFile")}>
+        <div className={cx("wrapper_uploadFile")}>
+          <h3 className={cx("wrapper_uploadFile_title")}>
+            {" "}
+            Upload ảnh để tiến hành dự đoán
+          </h3>
+          <div className={cx("wrapper_uploadFile_forInput")}>
+            <input
+              className={cx("wrapper_uploadFile_input")}
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+            />
+          </div>
+          <div className={cx("wrapper_uploadFile_forButton")}>
+            <button
+              className={cx("wrapper_uploadFile_button")}
+              onClick={handleOnClickUploadFile}
+            >
+              {" "}
+              Phân tích{" "}
+            </button>
+          </div>
+          <div className={cx("wrapper_imageUpload_parent")}>
+            {imageSrc && (
+              <img
+                className={cx("wrapper_imageUpload")}
+                src={imageSrc}
+                alt="preview"
+              />
+            )}
+          </div>
+        </div>
       </div>
-      <div className={cx("result")}>
-        <FaMicroscope /> Dự đoán bệnh: <strong>{response.prediction}</strong>
-        <span className={cx("badge")}>{response.confidence.toFixed(2)}%</span>
-      </div>
+      <div className={cx("wrapper")}>
+        <div className={cx("title")}>
+          <FaLeaf className={cx("icon")} />
+          Phát hiện bệnh lá xoài
+        </div>
+        <div className={cx("result")}>
+          <FaMicroscope /> Dự đoán bệnh: <strong>{response.prediction}</strong>
+          <span className={cx("badge")}>{response.confidence.toFixed(2)}%</span>
+        </div>
 
-      <div className={cx("chart")}>
-        <ResponsiveContainer width="100%" height={350}>
-          <BarChart data={chartData}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" tick={{ fontSize: 12 }} angle={-15} />
-            <YAxis domain={[0, 100]} />
-            <Tooltip />
-            <Bar dataKey="value" fill="#6c5ce7" radius={[8, 8, 0, 0]} />
-          </BarChart>
-        </ResponsiveContainer>
+        <div className={cx("chart")}>
+          <ResponsiveContainer width="100%" height={350}>
+            <BarChart data={chartData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" tick={{ fontSize: 12 }} angle={-15} />
+              <YAxis domain={[0, 100]} />
+              <Tooltip />
+              <Bar dataKey="value" fill="#6c5ce7" radius={[8, 8, 0, 0]} />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </div>
   );
